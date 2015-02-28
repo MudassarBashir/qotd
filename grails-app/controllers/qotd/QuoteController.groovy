562473ddd5abc89e18c303b286d0341735257929
Moe B <moeb59@gmail.com>
2015-02-28 17:15:46 -0500

    Added a layout named quote for the random gsp.

--- grails-app/controllers/qotd/QuoteController.groovy
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -2,5 +2,17 @@ package qotd
 
 class QuoteController {
 
+	static defaultAction = "home" /* Double quote is important here. Single quote didn't work! */
+
     def index() { }
+
+    def home() {
+		render "<h1>Real Programmers do not eat Quiche</h1>"
+	}
+
+	def random() {
+		def staticAuthor = "Anonymous"
+		def staticContent = "Real Programmers don't eat much quiche"
+		[author: staticAuthor, content: staticContent]
+	}
 }
