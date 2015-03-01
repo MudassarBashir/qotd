d7e6b5fc89955f23ce8ccc012370e20ca760a665
Moe B <moeb59@gmail.com>
2015-03-01 12:32:50 -0500

    Creating a quote on the fly and passing it to a gsp. Not quite random yet.

--- grails-app/controllers/qotd/QuoteController.groovy
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -11,8 +11,8 @@ class QuoteController {
 	}
 
 	def random() {
-		def staticAuthor = "Anonymous"
-		def staticContent = "Real Programmers don't eat much quiche"
-		[author: staticAuthor, content: staticContent]
+		def staticQuote = new Quote(author: "Anonymous",
+									content: "Real Programmers don't eat much Quiche")
+		[quote : staticQuote]
 	}
 }
--- grails-app/views/quote/random.gsp
+++ grails-app/views/quote/random.gsp
@@ -3,7 +3,9 @@
 		<title>Random Quote</title>
 	</head>
 	<body>
-		<q>${content}</q>
-		<p>${author}</p>
+		<div id="quote">
+			<q>${quote.content}</q>
+			<p>${quote.author}</p>
+		</div>
 	</body>
 </html>
